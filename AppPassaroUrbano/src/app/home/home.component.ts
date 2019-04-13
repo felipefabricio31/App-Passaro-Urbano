955e68a122f86ff20eefc58c3542e6c1ac2ef328
Felipe Souza <felipe.fabricio.m3@gmail.com>
2019-04-12 22:20:01 -0300

    Forma errada de utilizar um serviço dentro do angular.

--- AppPassaroUrbano/src/app/home/home.component.ts
+++ AppPassaroUrbano/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service'
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,10 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit()
+  {
+    let Ofertas: OfertasService = new OfertasService()
+    console.log(Ofertas.getOfertas())
   }
 
 }
---
+++ AppPassaroUrbano/src/app/ofertas.service.ts
@@ -0,0 +1,8 @@
+export class OfertasService
+{
+    public getOfertas(): Array<string>
+    {
+        let ofertas = ["Oferta01", "Oferta02", "Oferta03"]
+        return ofertas
+    }
+}
