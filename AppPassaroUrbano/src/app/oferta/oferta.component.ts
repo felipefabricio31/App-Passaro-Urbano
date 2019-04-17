56379a092905d86fb72961d014e06b8a3c250d94
Felipe Souza <felipe.fabricio.m3@gmail.com>
2019-04-17 11:36:17 -0300

    Trabalhando com snapshot para recuperar parâmetros da rotas.

--- AppPassaroUrbano/src/app/oferta/oferta.component.ts
+++ AppPassaroUrbano/src/app/oferta/oferta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-oferta',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfertaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+   }
 
   ngOnInit() {
+    console.log('id recuperado da rota: ', this.route.snapshot.params['id'])
   }
-
 }
+
+/*
+O Angular Router permite que você recupere facilmente parâmetros do URL, 
+que é uma funcionalidade essencial que é exigida pela maioria dos aplicativos da web. 
+Você pode usar os dois modos: o paramMapobservável ou o instantâneo, mas o segundo requer 
+que você tenha cuidado ao reutilizar componentes. 
+*/
