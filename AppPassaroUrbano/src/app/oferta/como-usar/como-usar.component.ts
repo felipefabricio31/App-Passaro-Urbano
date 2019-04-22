ce906bd8a7d4492949751ac928591dcb88517e1a
Felipe Souza <felipe.fabricio.m3@gmail.com>
2019-04-22 10:24:30 -0300

    Recuperando dados 'Como Usar' via HttpRequest.

--- AppPassaroUrbano/src/app/oferta/como-usar/como-usar.component.ts
+++ AppPassaroUrbano/src/app/oferta/como-usar/como-usar.component.ts
@@ -1,18 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ActivationEnd } from '@angular/router'
+import { OfertasService } from '../../ofertas.service'
+import { Oferta } from 'src/app/shared/oferta.model';
 
 @Component({
   selector: 'app-como-usar',
   templateUrl: './como-usar.component.html',
-  styleUrls: ['./como-usar.component.css']
+  styleUrls: ['./como-usar.component.css'],
+  providers: [ OfertasService ]
 })
 export class ComoUsarComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  public comoUsar: string = ''
+
+  constructor(
+    private route: ActivatedRoute,
+    private ofertaService: OfertasService
+    ) { }
 
   ngOnInit() {
     
-    console.log(this.route.parent.snapshot.params['id'])
+    console.log('ID da rota Pai: ', this.route.parent.snapshot.params['id'])
+
+    this.ofertaService.getComoUsarOfertaPorId(this.route.parent.snapshot.params['id'])
+    .then((descricao: string) => {
+      this.comoUsar = descricao
+    })
   }
 
 }
