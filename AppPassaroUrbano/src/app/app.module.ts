e652fb15e399882d894b1e34a3654e323919753e
Felipe Souza <felipe.fabricio.m3@gmail.com>
2019-05-02 23:05:02 -0300

    Criando o componente Ordem Compra e adicionando a classe de Rotas.

--- AppPassaroUrbano/src/app/app.module.ts
+++ AppPassaroUrbano/src/app/app.module.ts
@@ -18,7 +18,8 @@ import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 
-import { DescricaoReduzida } from './util/descricao-reduzida.pipe'
+import { DescricaoReduzida } from './util/descricao-reduzida.pipe';
+import { OrdemCompraComponent } from './ordem-compra/ordem-compra.component'
 
 // the second parameter 'fr' is optional
 registerLocaleData(localePt, 'pt');
@@ -34,7 +35,8 @@ registerLocaleData(localePt, 'pt');
     OfertaComponent,
     ComoUsarComponent,
     OndeFicaComponent,
-    DescricaoReduzida
+    DescricaoReduzida,
+    OrdemCompraComponent
   ],
   imports: [
     BrowserModule,
